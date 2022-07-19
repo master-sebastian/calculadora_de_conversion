4af115e60df8d136db1249a9bcede996a12d6cc4
Sebastian Gutierrez <orjuela.desarrollador@gmail.com>
2022-07-19 00:28:40 -0500

    recolectar datos de clientes en excel

--- Librery/conversores.py
+++ Librery/conversores.py
@@ -11,18 +11,21 @@ with open(os.path.join(base_path[0], "db", "JSON", "nombre_dolar.JSON")) as f:
 with open(os.path.join(base_path[0], "db", "JSON", "coleccion_money.JSON")) as f:
    config_coleccion_de_monedas = json.load(f)
 
-def __conversion_moneda(tipo_moneda):
+def __conversion_moneda(tipo_moneda, cantidad=None):
     existe_moneda = False
     for moneda_actual in config_coleccion_de_monedas: 
         if(str(moneda_actual["id"]) == tipo_moneda):
             try:
-                cantidad = float(input("Ingrese la cantidad a convertir: "))
+                if(cantidad == None):
+                    cantidad = float(input("Ingrese la cantidad a convertir: "))
+                else:
+                    cantidad = float(cantidad)
             except:
                 print("Cantidad no es validad")
-                exit()
+                return None
             if cantidad < 0:
                 print("Cantidad no valida")
-                exit()
+                return None
             conversion = cantidad * moneda_actual["factor_de_conversion_dolar"]
             mensaje = moneda_actual["nombre"][("plural", "singular")[cantidad == 1]].replace("[cantidad]", str(cantidad)) + " equivale a " + nombre_dolar[("plural", "singular")[conversion == 1]].replace("[cantidad]", str(conversion))
             existe_moneda = True
@@ -31,13 +34,15 @@ def __conversion_moneda(tipo_moneda):
         mensaje = "Ese tipo de moneda no esta configurada en este equipo"
     return mensaje
 
-def obtener_dato_por_consola():
+def obtener_dato_por_consola(tipo_moneda=None, cantidad=None):
+    if tipo_moneda != None:
+        return __conversion_moneda(tipo_moneda, cantidad)
     print("Monedas posibles a convertir:")
     for moneda_actual in config_coleccion_de_monedas:
         print(str(moneda_actual["id"]) + " - "+ moneda_actual["tipo"])    
     
     tipo_moneda = input("Ingrese el tipo de modena que desea convertir: ")
-    return __conversion_moneda(tipo_moneda) 
+    return __conversion_moneda(tipo_moneda, cantidad)
 
 
 
--- Run/main.py
+++ Run/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import csv
 
 base_path = os.path.dirname(os.path.abspath(__file__)).rsplit(os.sep, 1)
 
@@ -7,4 +8,10 @@ sys.path.insert(0, os.path.join(base_path[0], "Librery"))
 
 from conversores import obtener_dato_por_consola
 
-print(obtener_dato_por_consola())
+clientes = []
+with open(os.path.join(base_path[0], "db", "CSV", "Clientes.csv"), 'r') as f:
+    for cliente in f:
+        clientes.append(cliente.replace('\n','').split(";"))
+
+for cliente in clientes:
+    print(obtener_dato_por_consola(cliente[2], cliente[3]))
